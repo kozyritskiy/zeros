5ca616ccde41ffb32e3852b4cc2e0660185aba4b
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-09-18 20:53:27 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let zerosCount = 0;
+  while (number >= 5) {
+    number /= 5;
+    zerosCount += Math.floor(number);
+  }
+  return zerosCount;
 }
